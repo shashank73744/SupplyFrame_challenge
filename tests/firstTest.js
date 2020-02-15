f67200df8870ff0ad6ee90f98c94db691719b6a3
Shashank <shashank73744@gmail.com>
2020-02-15 15:38:42 -0800

    Added more test cases

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,10 +1,14 @@
 module.exports = {
     demoTest: function(browser) {
-      browser.init("http://localhost:8000/1");
+      browser.init("http://localhost:8000");
   
       browser
         .assert.not.elementPresent('.not_present') // previously .assert.elementNotPresent()
-        .assert.not.urlContains('https://');
+        .assert.not.urlContains('https://')
+        .assert.elementPresent("#dogBreed")
+        .assert.elementPresent("#dogImage")
+        .assert.domPropertyEquals('#dogBreed', 'classList', ['imageFrame'])
+        .assert.domPropertyEquals('#dogImage', 'classList', ['paginationindex']);
   
       // ...
     }
